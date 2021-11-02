6650890f997a81d2424f4a3bdbcb262172642f5c
Cha-Sang-Jun <scha39423@gmail.com>
2021-11-02 15:19:40 +0900

    21/11/02

--- sql/2021.11.01_SubQuery.sql
+++ sql/2021.11.01_SubQuery.sql
@@ -9,31 +9,30 @@ select dname, emp.deptno
 from emp, dept
 where emp.deptno = dept.deptno and ename ='SCOTT';
 
+-- SCOTT 사원의 부서 이름    
+-- 1. 내부 쿼리를 먼저 작성 : SCOTT 사원의 부서 번호
+select deptno from emp where ename = 'SCOTT';
+-- 2. 외부 쿼리 작성 : 부서 번호로 부서이름을 검색
 select dname 
 from dept 
 where deptno = (
     select deptno from emp where ename = 'SCOTT');
-    
--- SCOTT 사원의 부서 이름    
--- 1. 내부 쿼리를 먼저 작성 : SCOTT 사원의 부서 번호
--- 2. 외부 쿼리 작성 : 부서 번호로 부서이름을 검색
 
-select deptno from emp where ename = 'SCOTT';
-
-select deptno, dname from dept where deptno > 20;
 
 -- 마당서점의 고객별 판매액을 보이시오(결과는 고객이름과 고객별 판매액 출력)
+-- JOIN 이용
+select o.custid, c.name, sum(o.saleprice)
+from orders o, customer c
+where o.custid = c.custid
+group by o.custid, c.name;
+
+-- SUB QUERY
 select custid,
     (select name from customer c where o.custid = c.custid) as name,
     sum(saleprice)
 from orders o
 group by custid;
 
--- JOIN 이용
-select o.custid, c.name, sum(o.saleprice)
-from orders o, customer c
-where o.custid = c.custid
-group by o.custid, c.name;
 
 -- 사원의 이름과 부서 이름을 출력
 select ename, deptno,
@@ -69,3 +68,118 @@ group by custid;
 -- 30번 부서의 인라인 뷰를 만들어서 사용
 select *
 from (select * from emp where deptno = 30);
+
+select e.ename, e.deptno, s.ename
+from emp e, emp s
+where e.deptno = s.deptno and s.ename='SCOTT';
+
+----------------------------------------------------------------
+
+-- 2021.11.02
+
+-- 중첩 질의
+
+-- 평균 급여보다 더 많은 급여를 받는 사원을 검색하는 문장
+select avg(sal) from emp;
+-- 평균급여 : 2073.214285714285714285714285714285714286
+select ename, sal
+from emp
+where sal > 2073.214285714285714285714285714285714286;
+
+select ename, sal
+from emp
+where sal > (select avg(sal) from emp);
+
+
+-- 평균 주문금액 이하의 주문에 대해서 주문번호와 금액을 보이시오. 
+-- 평균 주문 금액 (sub query)
+select avg(saleprice) from orders;
+-- 주문금액 이하의 주문에 대해서 주문번호와 금액
+select orderid, saleprice
+from orders
+where saleprice <= (select avg(saleprice) from orders);
+
+-- 각 고객의 평균 금액보다 큰 금액의 주문 내역에 대해서 주문번호, 고객번호, 금액을 보이시오
+-- 각 고객의 평균 주문금액
+select avg(saleprice) from orders where custid = 1;
+-- 조건 : 각 고객의 평균 주문금액보다 큰 금액의 주문 내역
+-- 주문 내역에 대해서 주문번호, 고객번호, 금액
+select custid, orderid, saleprice
+from orders o
+where saleprice > (select avg(saleprice) from orders OA where OA.custid = O.custid);
+
+
+-- 다중행 서브 쿼리
+-- IN, SOME, ANY, ALL, EXISTS
+
+-- 3000 이상 받는 사원이 소속된 부서와 동일한 부서에서 근무하는 사원
+select ename, sal, deptno  
+from emp   
+where deptno in
+    (select distinct deptno from emp where sal >= 3000);
+    
+
+-- 대한민국에 거주하는 고객에게 판매한 도서의 총 판매액을 구하시오.
+-- 대한민국에 거주하는 고객
+select custid from customer where address like '%대한민국%';
+-- 도서 총 판매액
+select sum(saleprice) from orders;
+
+select sum(saleprice)
+from orders
+where custid in
+    (select custid from customer where address like '%대한민국%');
+    
+
+-- 3번 고객이 주문한 도서의 최고 금액보다 더 비싼 도서를 구입한 주문의 고객번호와 금액을 보이시오
+-- 3번 고객의 주문 도서의 금액들
+select saleprice from orders where custid = 3;
+-- 더 비싼 도서를 구입한 주문의 주문번호와 금액
+select orderid, saleprice
+from orders
+where saleprice > all
+    (select saleprice from orders where custid = 3);
+    
+
+-- 부서번호가 30번인 사원들의 급여 중 가장 작은 값(950)보다 많은 급여를 받는 사원의 이름, 급여를 출력
+-- 부서번호가 30번인 사원들의 급여
+select sal from emp where deptno = 30;
+-- 그 중 가장 작은 값 (필요 없는 부분)
+select min(sal) from emp where deptno = 30;
+-- 그 보다 많은 급여를 받는 사원의 이름, 급여
+select ename, deptno,   sal
+from emp
+where sal > any
+    (select sal from emp where deptno = 30);
+    
+
+-- 30번 소속 사원들 중에서 급여를 가장 많이 받는 사원보다 더 많은 급여를 받는 사람의 이름, 급여를 출력하는 쿼리문을 작성
+-- 30번 소속 사원들의 급여
+select sal from emp where deptno = 30;
+-- 그 중 급여를 가장 많이 받는 사원
+select max(sal) from emp where deptno = 30;
+-- 그 보다 더 많은 급여를 받는 사원의 이름, 급여
+select ename, deptno, sal
+from emp
+where sal > all
+    (select sal from emp where deptno = 30);
+
+
+-- EXISTS 연산자로 대한민국에 거주하는 고객에게 판매한 도서의 총 판매액을 구하시오
+-- 대한민국에 거주하는 고객
+select custid from customer where address like '%대한민국%';
+-- 그 고객에게 판매한 도서의 총 판매액
+select sum(saleprice)
+from orders o
+where exists 
+    (select * from customer c where address like '%대한민국%' and c.custid = o.custid);
+
+
+
+
+
+
+
+
+
+
---
+++ sql/subquery_exam6.sql
@@ -0,0 +1,142 @@
+
+-- 2021.11.02
+
+-- subquery_exam6
+
+--43. 사원 번호가 7788인 사원과 담당 업무가 같은 사원을 표시(사원 이름과 담당업무)하시오.
+-- 사원번호가 7788인 사원의 담당업무
+select ename, empno, job from emp where empno = 7788;
+-- 그와 업무가 같은 사원
+select ename, empno, job
+from emp
+where job = 
+    (select job from emp where empno = 7788);
+
+--44. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시하시오. 사원이름과 감당 업무
+-- 사원번호가 7499인 사원의 급여
+select ename, empno, sal from emp where empno = 7499;
+-- 그 사원의 급여보다 더 많이 받는 사원
+select ename, job, sal
+from emp
+where sal >
+    (select sal from emp where empno = 7499);
+    
+--45. 최소급여를 받는 사원의 이름, 담당업무 및 급여를 표시하시오. (그룹함수 사용)
+-- 최소급여
+select min(sal) from emp;
+
+select ename, job, sal
+from emp
+where sal = 
+    (select min(sal) from emp);
+
+--46. 평균급여가 가장 적은 직급의 직급 이름과 직급의 평균을 구하시오.
+select job, avg(sal)
+from emp
+group by job
+having avg(sal) =
+    (select min(avg(sal)) from emp group by job);
+
+
+--47. 각 부서의 최소 급여를 받는 사원의 이름, 급여, 부서번호를 표시하시오.
+select ename, sal, deptno
+from emp
+where sal = 
+    (select min(sal) from emp);
+
+--48. 담당업무가 ANALYST 인 사원보다 급여가 적으면서 업무가 ANALYST가 아닌 사원들을 표시(사원번호, 이름, 담당 업무, 급여)하시오.
+select empno, ename, job, sal
+from emp
+where sal <= all
+    (select sal from emp where job = 'ANALYST') and job != 'ANALYST';
+    
+--49. 부하직원이 없는 사원의 이름을 표시하시오.
+select ename, mgr
+from emp
+where mgr =
+    (select min(mgr) from emp);
+
+--50. 부하직원이 있는 사원의 이름을 표시하시오.
+select ename, mgr
+from emp
+where mgr >
+    (select min(mgr) from emp);
+    
+--51. BLAKE와 동일한 부서에 속한 사원의 이름과 입사일을 표시하는 질의를 작성하시오. ( 단 BLAKE는 제외 )
+--select ename, hiredate
+--from emp
+--where deptno =
+--    (select deptno from emp where ename like 'BLAKE')
+----having not ename = 'BLAKE';
+
+--52. 급여가 평균 급여보다 많은 사원들의 사원 번호와 이름을 표시하되 결과를 급여에 대해서 오름차순으로 정렬하시오.
+select ename, empno
+from emp
+where sal >
+    (select avg(sal) from emp)
+order by sal asc;
+
+--53. 이름에 K가 포함된 사원과 같은 부서에서 일하는 사원의 사원 번호와 이름을 표시하시오.
+-- 이름에 K가 포함된 사원의 부서
+select ename, deptno from emp where ename like '%K%';
+
+select ename, empno
+from emp
+where deptno = any
+    (select deptno from emp where ename like '%K%');
+
+--54. 부서위치가 DALLAS인 사원의 이름과 부서번호 및 담당업무를 표시하시오.
+select ename, deptno, job
+from emp
+where deptno = any
+    (select deptno from dept where loc like 'DALLAS');
+    
+--55. KING에게 보고하는 사원의 이름과 급여를 표시하시오.
+select ename, sal
+from emp
+where mgr =
+    (select empno from emp where ename like 'KING');
+    
+--56. RESEARCH 부서의 사원에 대한 부서번호, 사원이름 및 담당 업무를 표시하시오.
+select deptno, ename, job
+from emp
+where deptno = any
+    (select deptno from dept where dname like 'RESEARCH');
+
+-- 57. 평균 월급보다 많은 급여를 받고 
+-- 이름에 M이 포함된 사원과 같은 부서에서 근무하는 사원의 
+-- 사원 번호, 이름, 급여를 표시하시오.
+
+select ename, deptno
+from emp 
+where sal >
+    (select avg(sal) from emp);
+    
+select deptno
+from emp
+where ename like '%M%';
+
+select empno, ename, sal
+from emp
+where exists
+    (select avg(sal) from where ename like '%M%');
+
+-- 58. 평균급여가 가장 적은 업무를 찾으시오.
+select job, avg(sal)
+from emp
+group by job
+having avg(sal) =
+    (select min(avg(sal)) from emp group by job);
+
+    
+-- 59. 담당업무가 MANAGER 인 사원이 소속된 부서와 동일한 부서의 사원을 표시하시오
+-- 담당업무가 MANAGER 인 사원의 부서
+select deptno, ename
+from emp
+where job like 'MANAGER';
+
+select ename
+from emp e
+where exists
+    (select * from emp m where job like 'MANAGER' and e.deptno = m.deptno);
+    
