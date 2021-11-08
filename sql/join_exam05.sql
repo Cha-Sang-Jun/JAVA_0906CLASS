2709e895bae951806f2c60e88bb522204edbc552
Cha-Sang-Jun <scha39423@gmail.com>
2021-11-08 20:49:06 +0900

    Update join_exam05.sql

--- sql/join_exam05.sql
+++ sql/join_exam05.sql
@@ -36,11 +36,25 @@ select c.name, b.bookname
 from book b, customer c, orders o
 where c.custid = o.custid and o.bookid = b.bookid;
 
---(12) 도서의가격(Book 테이블)과 판매가격(Orders 테이블)의 차이가 가장 많은 주문
+--(12) 도서의가격(Book 테이블)과 판매가격(Orders 테이블)의 차이가 가장 많은 주문 (x)
+select o.orderid, o.custid, o.bookid, o.saleprice, b.bookname, b.bookid, b.publisher, b.price
+from orders o, book b
+where o.bookid = b.bookid and (b.price - o.saleprice) =
+    (select max(b.price - o.saleprice) from orders o, book b where o.bookid = b.bookid);
 
+-- (13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름 (x)
+select c.name
+from customer c, orders o
+where o.custid = c.custid
+group by c.name
+having avg(saleprice) > (select avg(saleprice) from orders);
 
---3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
---(1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
-    
---(2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
+-- 3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
+-- (1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
+select c.name
+from customer c, orders o, book b
+where o.custid = c.custid and o.bookid = b.bookid and b.publisher in
+    (select b.publisher from customer c, orders o, book b 
+    where o.custid = c.custid and o.bookid = b.bookid and name = '박지성') and name != '박지성';
 
+-- (2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
