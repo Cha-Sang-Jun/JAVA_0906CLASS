6d94fd95f35033c08631ac9a8775b3c8b1e99115
Cha-Sang-Jun <scha39423@gmail.com>
2021-11-29 13:19:30 +0900

    21/11/29

--- html/member/member.js
+++ html/member/member.js
@@ -5,20 +5,29 @@ var members = [];
 
 
 window.onload = function () {
+
+    // 회원 리스트 갱신
+    setMemberList();
+
     // regForm 캐스팅 => onsubmit 이벤트 설정
     // 데이터 캐스팅 -> 객체 -> 배열에 저장
 
     // 폼 태그 캐스팅
     var regForm = document.getElementById('regForm');
+
+    // 이 변수들을 onsubmit 안에만 넣으면 핸들러 안에서만 사용 가능하다.
+    // 좀 더 넓게 쓸 수 있게 밖으로 빼준다.
+    var userId = document.querySelector('#userId');
+    var pw = document.querySelector('#pw');
+    var repw = document.querySelector('#repw');
+    var userName = document.querySelector('#userName');
+
     // 이벤트 설정 (이벤트 핸들러)
     regForm.onsubmit = function () {
 
         // alert('onsubmit!!');
         // 데이터 받아서 개체 생성
-        var userId = document.querySelector('#userId');
-        var pw = document.querySelector('#pw');
-        var repw = document.querySelector('#repw');
-        var userName = document.querySelector('#userName');
+
 
         // 사용자 입력 값의 유효성 검사
         // 아이디 검사 : 공백, 문자열의 갯수 3~8, 서버통신(비동기통신) 아이디 중복 여부
@@ -31,7 +40,7 @@ window.onload = function () {
             return false;
 
         } else if (!(userId.value.trim().length > 2 && userId.value.trim().length < 9)) {
-            document.querySelector('#msgId').innerHTML = '아이디는 3~8자리까지 가능합니다.';
+            document.querySelector('#msgId').innerHTML = '아이디는 3~8자리까지 가능';
             return false;
         }
 
@@ -41,7 +50,7 @@ window.onload = function () {
             document.querySelector('#msgPw').innerHTML = '비밀번호는 필수항목 입니다.';
             return false;
         } else if (!(pw.value.trim().length > 4 && pw.value.trim().length < 10)) {
-            document.querySelector('#msgPw').innerHTML = '비밀번호는 4자리 ~ 10자리까지 가능합니다.';
+            document.querySelector('#msgPw').innerHTML = '비밀번호는 4~ 10자리까지 가능';
             return false;
         }
 
@@ -59,7 +68,7 @@ window.onload = function () {
             document.querySelector('msgName').innerHTML = '이름 입력은 필수입니다.';
             return false;
         } else if (userName.value.trim().length < 2) {
-            document.querySelector('#msgName').innerHTML = '이름은 2자리 이상이여야 합니다.';
+            document.querySelector('#msgName').innerHTML = '이름은 2자리 이상';
         }
 
 
@@ -76,9 +85,28 @@ window.onload = function () {
         // this : 이벤트가 발생한 객체를 가리킨다. -> regForm
         this.reset();
 
+        // 입력 후 회원 리스트 갱신
+        setMemberList();
+
         return false;
     }
 
+    // 입력 폼에 focus가 들어가면 아래쪽 경고 메세지가 삭제되도록
+    userId.addEventListener('focus', function () {
+        document.querySelector('#msgId').innerHTML = '';
+    });
+
+    pw.addEventListener('focus', function () {
+        document.querySelector('#msgPw').innerHTML = '';
+    });
+
+    repw.addEventListener('focus', function () {
+        document.querySelector('#msgRePw').innerHTML = '';
+    });
+
+    userName.addEventListener('focus', function () {
+        document.querySelector('#msgName').innerHTML = '';
+    });
 
     // 이벤트 설정 : 입력, 수정, 삭제
 
@@ -93,5 +121,38 @@ function Member(userId, pw, userName) {
     this.userId = userId;
     this.pw = pw;
     this.userName = userName;
+}
+
+// JSON -> JS Object {}
+
+// 회원 리스트 출력을 위한 기능
+function setMemberList() {
+
+    // 화면을 갱신하는 HTML 생성
+    var list = '';
+    list += '<tr>';
+    list += ' <th> 순번(index) </th>'
+    list += ' <th> 아이디 </th> '
+    list += ' <th> 비밀번호 </th>'
+    list += ' <th> 이름 </th>'
+    list += ' <th> 관리 </th>'
+    list += '</tr>'
+
+    if (members.length == 0) {
+        list += '<tr> <td colspan="5"> 회원의 정보가 존재하지 않습니다. </td></tr>'
+    } else {
+        for (let i = 0; i < members.length; i++) {
+            list += '<tr>';
+            list += '  <td> ' + i + ' </td>'
+            list += '  <td> ' + members[i].userId + ' </td>'
+            list += '  <td> ' + members[i].pw + ' </td>'
+            list += '  <td> ' + members[i].userName + ' </td>'
+            list += '  <td>  <a href="#"> 수정 </a> | <a href="#"> 삭제 </a>  </td>'
+            list += '</tr>';
+        }
+    }
+
+    // tbody 캐스팅
+    document.querySelector('#memberList>table>tbody').innerHTML = list;
 }
 
