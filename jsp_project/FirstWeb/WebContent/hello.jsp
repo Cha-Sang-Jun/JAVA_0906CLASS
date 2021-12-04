c22162ba818c5a5d1799af3edcc0c25a28058511
Cha-Sang-Jun <scha39423@gmail.com>
2021-12-04 19:00:43 +0900

    21/12/03

---
+++ jsp_project/FirstWeb/WebContent/hello.jsp
@@ -0,0 +1,24 @@
+<%@page import="java.time.LocalDateTime"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.util.Date"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%   // --> 출력 (모니터에 출력하는게 아니라 명령어 객체를 toString 텍스트로 출력)
+		// java 코드를 작성
+		// java.util.Date now1 = new java.util.Date();
+		Date now1 = new Date();
+	%>
+
+	<%=now1%>
+	<h1>
+		<%=LocalDateTime.now()%>
+	</h1>
+	<h1>처음 작성한 JSP 파일</h1>
+</body>
+</html>
---
+++ jsp_project/FirstWeb/src/first/HelloServlet.java
@@ -0,0 +1,21 @@
+package first;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override // get 방식으로 들어오는 요청처리
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Get 요청 처리!!!");
+	}
+
+	@Override // post 방식으로 들어오는 요청 처리
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+
+}
