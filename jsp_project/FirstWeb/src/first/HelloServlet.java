859d9d16f33b39c92d5980e37f6df6ce3099ede4
Cha-Sang-Jun <scha39423@gmail.com>
2021-12-06 13:21:07 +0900

    21/12/06

--- jsp_project/FirstWeb/src/first/HelloServlet.java
+++ jsp_project/FirstWeb/src/first/HelloServlet.java
@@ -9,13 +9,26 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
-	@Override // get 방식으로 들어오는 요청처리
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("Get 요청 처리!!!");
+		
+		resp.getWriter().println("<html>");
+		resp.getWriter().println("<head>");
+		resp.getWriter().println("<title>Servlet</title>");
+		resp.getWriter().println("</head>");
+		resp.getWriter().println("<body>");
+		resp.getWriter().println("<h1>Get Request !!!</h1>");
+		resp.getWriter().println("</body>");
+		resp.getWriter().println("</html>");
+		
 	}
 
-	@Override // post 방식으로 들어오는 요청 처리
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 	}
 
+	
 }
+
